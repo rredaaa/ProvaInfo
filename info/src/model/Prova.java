82974a48d3053472a4d66e43b2ab0302bbd1d63e
CalebZ <czuccotti.s@itiscardanopv.edu.it>
2024-05-22 09:32:20 +0200

    info22
    ho aggiunto il model

---
+++ info/src/model/Prova.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author czuccotti74335
+ */
+public class Prova {
+    
+    private String name,surname;
+
+    public Prova(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+  
+    
+    
+    
+}
