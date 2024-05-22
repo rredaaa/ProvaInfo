f04b17962bd823791b2fc49536776c787c806a43
belloreale <lbelfiore.s@itiscardanopv.edu.it>
2024-05-22 09:39:08 +0200

    25
    rrrrr

---
+++ info/src/view/MyFrame.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Utente
+ */
+public class MyFrame extends JFrame{
+
+    public MyFrame() {
+        
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+    }
+    
+    
+    
+}
